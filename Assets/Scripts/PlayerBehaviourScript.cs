074de082754d69fb1e5f044f2f2bc98c788e3859
Anton Bogomaz <bogomaz97@gmail.com>
2017-01-28 17:57:25 +0200

    Hallo world in gaming

--- Assets/Scripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerBehaviourScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2 (50, 50);
+
+	private Vector2 movement = new Vector2(1f,2f);
+	private Rigidbody2D rgdb2D = new Rigidbody2D();
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		rgdb2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY
+		);
+	}
+
+	void FixedUpdate()
+	{
+		rgdb2D.velocity = movement;
 	}
 }
